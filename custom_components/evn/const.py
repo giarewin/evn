998fa4573ad1f5090eced8d841a707ebe9404d71
giarewin <acc@giare.win>
2025-10-18 20:08:14 +0700

    Create const.py

---
+++ custom_components/evn/const.py
@@ -0,0 +1,33 @@
+DOMAIN = "evn"
+
+# Inputs (setup)
+CONF_FORWARD_TOTAL = "forward_total_entity"
+CONF_REVERSE_TOTAL = "reverse_total_entity"
+CONF_PREFIX        = "prefix"
+
+# Options (runtime)
+CONF_OUTPUT_DIR    = "output_dir"        # mặc định: /config/www/energy
+CONF_INTERVAL_MIN  = "interval_minutes"  # mặc định: 5
+CONF_ROUND         = "round_decimals"    # mặc định: 3
+
+# Seeds (căn mốc hiển thị tại thời điểm hiện tại) - cho phép số hoặc từ sensor khác
+CONF_SEED_BUY_DAY        = "seed_buy_day"
+CONF_SEED_BUY_MONTH      = "seed_buy_month"
+CONF_SEED_BUY_YEAR       = "seed_buy_year"
+CONF_SEED_SELL_DAY       = "seed_sell_day"
+CONF_SEED_SELL_MONTH     = "seed_sell_month"
+CONF_SEED_SELL_YEAR      = "seed_sell_year"
+
+CONF_SEED_BUY_DAY_ENTITY   = "seed_buy_day_entity"
+CONF_SEED_BUY_MONTH_ENTITY = "seed_buy_month_entity"
+CONF_SEED_BUY_YEAR_ENTITY  = "seed_buy_year_entity"
+CONF_SEED_SELL_DAY_ENTITY  = "seed_sell_day_entity"
+CONF_SEED_SELL_MONTH_ENTITY= "seed_sell_month_entity"
+CONF_SEED_SELL_YEAR_ENTITY = "seed_sell_year_entity"
+
+DEFAULT_OUTPUT_DIR = "www/energy"
+DEFAULT_INTERVAL   = 5
+DEFAULT_ROUND      = 3
+
+STORAGE_KEY        = "storage"
+STORAGE_VERSION    = 1
