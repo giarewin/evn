2dbbc7d70f6d303a0c24022a003986d584916c0b
giarewin <acc@giare.win>
2025-10-18 19:58:19 +0700

    Create const.py

---
+++ custom_components/evn/const.py
@@ -0,0 +1,26 @@
+DOMAIN = "evn"
+
+# Inputs
+CONF_FORWARD_TOTAL = "forward_total_entity"
+CONF_REVERSE_TOTAL = "reverse_total_entity"
+CONF_PREFIX        = "prefix"
+
+# Options
+CONF_OUTPUT_DIR    = "output_dir"        # mặc định: /config/www/energy
+CONF_INTERVAL_MIN  = "interval_minutes"  # mặc định: 5
+CONF_ROUND         = "round_decimals"    # mặc định: 3
+
+# Seeds (nhập ở Options UI để căn lại mốc tại thời điểm hiện tại)
+CONF_SEED_BUY_DAY   = "seed_buy_day"
+CONF_SEED_BUY_MONTH = "seed_buy_month"
+CONF_SEED_BUY_YEAR  = "seed_buy_year"
+CONF_SEED_SELL_DAY  = "seed_sell_day"
+CONF_SEED_SELL_MONTH= "seed_sell_month"
+CONF_SEED_SELL_YEAR = "seed_sell_year"
+
+DEFAULT_OUTPUT_DIR = "www/energy"
+DEFAULT_INTERVAL   = 5
+DEFAULT_ROUND      = 3
+
+STORAGE_KEY        = "storage"
+STORAGE_VERSION    = 1
